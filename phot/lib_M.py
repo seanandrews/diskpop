c605033791b71a81a6fd561cc9239c804b35a9a9
Sean Andrews <sandrews@cfa.harvard.edu>
2016-06-01 16:36:41 -0400

    test for grid point matches; cleanup interpolate code

--- phot/lib_M.py
+++ phot/lib_M.py
@@ -10,10 +10,11 @@ import matplotlib.pyplot as plt
 import sys
 
 # parse inputs
-in_band  = 'Rc'
+in_band  = np.array(['Rc', 'Ic', 'J2m'])
 in_teff  = 4800.
 in_logg  = 4.5
 in_zstar = 0.0
+in_rstar = 1.0
 
 # import the model library
 lib = np.load('maglib.npz')
@@ -23,26 +24,31 @@ zstar = lib['zstar']
 mlib  = lib['mlib']
 band  = lib['band']
 
+# extract the relevant bands
+maglib = mlib[:,:,:,np.in1d(band, in_band)]
 
-# temp: pluck out the in_band
-maglib = np.squeeze(mlib[:,:,:,(band == in_band)])
+# if the input parameters are *exactly* on the model library grid, then just
+# use the grid; if not, then do the interpolation
+if (np.any(teff == in_teff) & \
+    np.any(logg == in_logg) & \
+    np.any(zstar == in_zstar)):
 
+    mag_int = np.squeeze(maglib[teff==in_teff, logg==in_logg, zstar==in_zstar])
 
-# interpolator
-fint = RegularGridInterpolator((teff, logg, zstar), maglib)
+else:
 
+    # create the interpolator function (here using trilinear interpolation)
+    fint = RegularGridInterpolator((teff, logg, zstar), maglib)
 
-pts = np.array([in_teff, in_logg, in_zstar])
-MM = fint(pts)
-print(np.inf-4.)
-print(logg)
-print(zstar)
+    # calculate interpolated absolute magnitudes at stellar surface
+    mag_int = np.squeeze(fint(np.array([in_teff, in_logg, in_zstar])))
 
-#plt.axis([4000, 5000, -40, -35])
-plt.axis([-0.5, 6., -40, -35])
-#plt.plot(teff, maglib[:,(logg == 4.0),(zstar == -0.5)], 'ob')
-print(np.shape(maglib[(teff == 4800.),:,(zstar == 0.0)]))
-plt.plot(logg, np.squeeze(maglib[(teff == 4800.),:,(zstar == 0.0)]), 'og')
-#plt.plot(in_logg, MM, 'or')
+print(mag_int)
 
-plt.show()
+# put stellar radius in parsec units
+in_rstar *= 6.96e10/3.0857e18
+
+# convert to standard absolute magnitudes
+M = mag_int - 5.*np.log10(in_rstar) + 5.
+
+print(M)
